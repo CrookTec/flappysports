06b1359244f5ee6a2b8b7190e329bab8490f0612
Tanner Crook <tanner.crook@crooktec.com>
2023-06-21 09:46:45 -0600

    Added exports for package.

--- lib/flappysports.dart
+++ lib/flappysports.dart
@@ -1,7 +1,6 @@
 library flappysports;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'School.dart';
+export 'Event.dart';
+export 'ApiConnection.dart';
+
